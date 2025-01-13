8ee78b6868cc7b49a419e4f263ef3dd00ffbdcd7
pythonicforge <pythonicforge@gmail.com>
2025-01-13 20:19:57 +0530

    feat: add navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import "./assets/styles/main.scss";
+import AppRoutes from "./routes/Routes.js";
+import Navbar from "./components/Navbar";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import { BrowserRouter as Router } from "react-router-dom";
+
 function App() {
-  return <></>;
+  return (
+    <Router className="container">
+      <Navbar />
+      <AppRoutes />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </Router>
+  );
 }
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { FaLinkedin } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaDiscord } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <></>
-  )
+    <>
+      <section className="navbar-container container">
+        <div className="logo-container">
+          <Link className="logo-text" to="/">
+            nxtup
+          </Link>
+        </div>
+        <div className="social-links-container">
+          <ul className="links">
+            <li className="link">
+              <Link to="https://linkedin.com">
+                <FaLinkedin />
+              </Link>
+            </li>
+            <li className="link">
+              <Link to="https://x.com">
+                <FaXTwitter />
+              </Link>
+            </li>
+            <li className="link">
+              <Link to="https://discord.com">
+                <FaDiscord />
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
